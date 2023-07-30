8c6a359c5a0e90d9ef0ab29a09cc1944fed5c0a7
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2023-07-30 09:00:27 -0300

    feat:todo delete e completed  adding

--- src/components/TodoForm.vue
+++ src/components/TodoForm.vue
@@ -7,7 +7,7 @@ const todo=ref('')
 const store=useTodoListStore()
 
 
-function submitForm(todo)
+function addItemAndClear(todo)
 {
   if(todo.length ===0)
   {
@@ -22,7 +22,7 @@ function submitForm(todo)
 <template>
 
   <div>
-    <form @submit.prevent="submitForm(todo)">
+    <form @submit.prevent="addItemAndClear(todo)">
       <input v-model="todo" type="text"/>
       <button>
         Add
@@ -33,5 +33,31 @@ function submitForm(todo)
 </template>
 
 <style scoped>
-
+form {
+  margin-bottom: 15px;
+}
+input {
+  margin-top: 25px;
+  margin-bottom: 15px;
+  height: 20px;
+  width: 50%;
+}
+button {
+  margin-left: 30px;
+  background-color: #2f6089;
+  padding: 5px 10px;
+  border: none;
+  border-radius: 5px;
+  font-weight: 800;
+  color: white;
+  width: 15%;
+}
+.alert-div {
+  min-height: 25px;
+}
+.alert {
+  color: #d1495b;
+  font-size: 1em;
+  font-weight: 600;
+}
 </style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -9,19 +9,36 @@ const store = useTodoListStore();
 const { todoList } = storeToRefs(store);
 
 //PRA PEGAR AS ACTIONS, NÃO PRECISAMOS DO STORE TO REFS
-const { toggleCompleted } = store;
+const { toggleCompleted,deleteItem } = store;
 </script>
 
 <template>
   <div v-for="todo in todoList" :key="todo.id" class="item">
     <div class="content">
       <span :class="{ completed: todo.completed }">{{ todo.item }}</span>
-      <span @click.stop="toggleCompleted(todo.id)">&#10004;</span>
+      <div>
+        <span @click.stop="toggleCompleted(todo.id)">&#10004;</span>
+        <span @click="deleteItem(todo.id)" class="x">&#10060;</span>
+      </div>
     </div>
   </div>
 </template>
-
 <style scoped>
+span {
+  margin: 0 10px;
+  cursor: pointer;
+}
+.item {
+  display: flex;
+  justify-content: center;
+}
+.content {
+  display: flex;
+  font-size: 1.5em;
+  justify-content: space-between;
+  width: 80vw;
+  padding: 5px;
+}
 .completed {
   text-decoration: line-through;
 }
